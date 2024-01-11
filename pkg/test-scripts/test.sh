b26ceb1d30fa3c912657973f9a515b2f97a5ad68
Maarten Aertsen <maarten@nlnetlabs.nl>
2024-01-11 16:03:59 +0100

    Don't hardcode debian in test

--- pkg/test-scripts/test.sh
+++ pkg/test-scripts/test.sh
@@ -10,7 +10,8 @@ case $1 in
     curl -fsSL https://packages.nlnetlabs.nl/aptkey.asc | sudo gpg --dearmor | diff -q /usr/share/keyrings/nlnetlabs-archive-keyring.gpg -
 
     echo -e "\nPACKAGE INSTALL SHOULD NOW AUTHENTICATE:"
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/nlnetlabs-archive-keyring.gpg] https://packages.nlnetlabs.nl/linux/debian \
+    . /etc/os-release
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/nlnetlabs-archive-keyring.gpg] https://packages.nlnetlabs.nl/linux/${ID} \
     $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/nlnetlabs.list > /dev/null
     apt update
     apt-get install --simulate --yes routinator
