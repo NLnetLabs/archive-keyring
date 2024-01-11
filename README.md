c0f629b3f0e2a3f76a0c0bf25dac7cbd6f542c7f
Maarten Aertsen <maarten@nlnetlabs.nl>
2024-01-11 15:45:39 +0100

    Create nlnetlabs-archive-keyring package built upon Ploutos

---
+++ README.md
@@ -0,0 +1,3 @@
+# NLnet Labs archive keyring for Debian and derivatives
+
+Packaging using [Ploutos](https://github.com/NLnetLabs/ploutos/).
---
+++ pkg/test-scripts/test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+set -x
+
+case $1 in
+  post-install)
+    apt-get install -y curl
+    echo -e "\nINSTALLED KEYRING SHOULD MATCH ONLINE COPY:"
+    curl -fsSL https://packages.nlnetlabs.nl/aptkey.asc | sudo gpg --dearmor | diff -q /usr/share/keyrings/nlnetlabs-archive-keyring.gpg -
+
+    echo -e "\nPACKAGE INSTALL SHOULD NOW AUTHENTICATE:"
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/nlnetlabs-archive-keyring.gpg] https://packages.nlnetlabs.nl/linux/debian \
+    $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/nlnetlabs.list > /dev/null
+    apt update
+    apt-get install --simulate --yes routinator
+    ;;
+  post-upgrade)
+    ;;
+esac
