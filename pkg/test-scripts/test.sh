744618057ae78576b10d8db8780b19e6c6c8708f
Maarten Aertsen <maarten@nlnetlabs.nl>
2024-01-11 16:38:01 +0100

    First test failure without key

--- pkg/test-scripts/test.sh
+++ pkg/test-scripts/test.sh
@@ -9,10 +9,15 @@ case $1 in
     echo -e "\nINSTALLED KEYRING SHOULD MATCH ONLINE COPY:"
     curl -fsSL https://packages.nlnetlabs.nl/aptkey.asc | sudo gpg --dearmor | diff -q /usr/share/keyrings/nlnetlabs-archive-keyring.gpg -
 
+    echo -e "\nPACKAGE INSTALL SHOULD FAIL TO AUTHENTICATE WITHOUT SIGNED-BY LINE:"
+    echo "deb [arch=$(dpkg --print-architecture)] https://packages.nlnetlabs.nl/linux/${ID} $(lsb_release -cs) main" > /etc/apt/sources.list.d/nlnetlabs.list
+    apt update
+    ! apt-get install --simulate --yes routinator
+
     echo -e "\nPACKAGE INSTALL SHOULD NOW AUTHENTICATE:"
     . /etc/os-release
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/nlnetlabs-archive-keyring.gpg] https://packages.nlnetlabs.nl/linux/${ID} \
-    $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/nlnetlabs.list > /dev/null
+    $(lsb_release -cs) main" > /etc/apt/sources.list.d/nlnetlabs.list
     apt update
     apt-get install --simulate --yes routinator
     ;;
