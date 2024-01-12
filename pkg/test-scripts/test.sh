85bdaa44be89a0ce9efa118ac028f5cc3c4bb353
Maarten Aertsen <maarten@nlnetlabs.nl>
2024-01-12 11:50:28 +0100

    Improve tests
      - fix the negative test
      - improve descriptions
      - use wget (available)
      - turn on gpgv debugging

--- pkg/test-scripts/test.sh
+++ pkg/test-scripts/test.sh
@@ -5,21 +5,23 @@ set -x
 
 case $1 in
   post-install)
-    apt-get install -y curl
     echo -e "\nINSTALLED KEYRING SHOULD MATCH ONLINE COPY:"
-    curl -fsSL https://packages.nlnetlabs.nl/aptkey.asc | sudo gpg --dearmor | diff -q /usr/share/keyrings/nlnetlabs-archive-keyring.gpg -
+    wget -qO- https://packages.nlnetlabs.nl/aptkey.asc | gpg --dearmor | diff -q /usr/share/keyrings/nlnetlabs-archive-keyring.gpg -
+
+    # define $ID, which is 'debian', 'ubuntu', ..
+    . /etc/os-release
 
     echo -e "\nAPT UPDATE SHOULD FAIL TO AUTHENTICATE WITHOUT SIGNED-BY LINE:"
-    echo "deb [arch=$(dpkg --print-architecture)] https://packages.nlnetlabs.nl/linux/${ID} $(lsb_release -cs) main" > /etc/apt/sources.list.d/nlnetlabs.list
-    ! apt-get update || false
+    echo "deb [arch=$(dpkg --print-architecture)] https://packages.nlnetlabs.nl/linux/${ID} ${VERSION_CODENAME} main" > /etc/apt/sources.list.d/nlnetlabs.list
+    ! apt-get update -o Debug::Acquire::gpgv=1
 
-    echo -e "\nAND AUTHENTICATE WITH SIGNED-BY LINE:"
-    . /etc/os-release
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/nlnetlabs-archive-keyring.gpg] https://packages.nlnetlabs.nl/linux/${ID} \
-    $(lsb_release -cs) main" > /etc/apt/sources.list.d/nlnetlabs.list
-    apt-get update
+    echo -e "\nAPT UPDATE SHOULD SUCCEED TO AUTHENTICATE WITH SIGNED-BY LINE:"
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/nlnetlabs-archive-keyring.gpg] \
+    https://packages.nlnetlabs.nl/linux/${ID} ${VERSION_CODENAME} main" > /etc/apt/sources.list.d/nlnetlabs.list
+    apt-get update -o Debug::Acquire::gpgv=1
+    # when debugging -o Debug::Acquire::https=1 may prove useful too
 
-    echo -e "\nAND THUS ABLE TO INSTALL ROUTINATOR:"
+    echo -e "\nAPT INSTALL SHOULD NOW WORK:"
     apt-get install --simulate --yes routinator
     ;;
   post-upgrade)
